b16b9cede402eade6619df1aa827c708260c1477
Ryo Narisawa <ororoauto@gmail.com>
2021-06-23 00:29:49 +0900

    Commodity 更新UseCase　重複チェック追加

--- lib/domain/usecases/update_commodity_use_case.dart
+++ lib/domain/usecases/update_commodity_use_case.dart
@@ -1,6 +1,7 @@
 import 'package:compare_prices/data/providers.dart';
 import 'package:compare_prices/domain/entities/commodity.dart';
 import 'package:compare_prices/domain/entities/result.dart';
+import 'package:compare_prices/domain/exception/domain_exception.dart';
 import 'package:compare_prices/domain/repositories/commodity_repository.dart';
 import 'package:compare_prices/domain/usecases/use_case.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -9,8 +10,7 @@ final updateCommodityUseCaseProvider =
     Provider.autoDispose<UpdateCommodityUseCase>(
         (ref) => UpdateCommodityUseCase(ref.read));
 
-class UpdateCommodityUseCase
-    extends FutureUseCase<void, Commodity> {
+class UpdateCommodityUseCase extends FutureUseCase<void, Commodity> {
   final Reader _reader;
 
   late final CommodityRepository _commodityRepository =
@@ -20,6 +20,19 @@ class UpdateCommodityUseCase
 
   @override
   Future<Result<void>> call(Commodity params) {
-   return Result.guardFuture(() => _commodityRepository.updateCommodity(params));
+    return Result.guardFuture(() async {
+      final commodity =
+          await _commodityRepository.getEnabledCommodityByName(params.name);
+
+      // 別商品データに同名の商品名が存在した場合はエラー
+      if (commodity != null) {
+        if (commodity.id != params.id) {
+          throw DomainException("すでにそんざいしてます");
+        }
+        return;
+      }
+
+      _commodityRepository.updateCommodity(params);
+    });
   }
 }
