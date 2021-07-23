20eeee4887d441ec47ca0b4ea938933abd0c0d9d
Ryo Narisawa <ororoauto@gmail.com>
2021-07-23 22:01:25 +0900

    文言調整

--- lib/ui/price/commodity/commodity_price_list_page.dart
+++ lib/ui/price/commodity/commodity_price_list_page.dart
@@ -1,9 +1,7 @@
 import 'package:compare_prices/domain/entities/commodity.dart';
-import 'package:compare_prices/ui/assets/color/app_colors.dart';
 import 'package:compare_prices/ui/common/extensions/exception_type_extensions.dart';
 import 'package:compare_prices/ui/price/commodity/commodity_price_row.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -40,42 +38,19 @@ class CommodityPriceListPage extends HookWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.commodityPriceListTitle),
+        title: Text(commodity.name),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Text(
-              commodity.name,
-              style: TextStyle(fontSize: 30),
-            ),
-          ),
-          Container(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 4),
+        child: ListView.builder(
+            itemCount: commodityPrices.length,
             padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Row(
-              children: [
-                Text(
-                  AppLocalizations.of(context)!.commodityPriceListRanking,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: AppColors.primary),
-                ),
-              ],
-            ),
-          ),
-          Divider(color: AppColors.primary),
-          Expanded(
-            child: ListView.builder(
-                itemCount: commodityPrices.length,
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                itemBuilder: (context, index) {
-                  final row = commodityPrices[index];
-                  return CommodityPriceRow(row, () {
-                    viewModel.disablePurchaseResult(row.purchaseResultId);
-                  });
-                }),
-          ),
-        ],
+            itemBuilder: (context, index) {
+              final row = commodityPrices[index];
+              return CommodityPriceRow(row, () {
+                viewModel.disablePurchaseResult(row.purchaseResultId);
+              });
+            }),
       ),
       floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
