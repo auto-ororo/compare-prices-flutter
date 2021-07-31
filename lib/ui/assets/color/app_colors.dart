d73fd5761e29cd5e358d97745ef761522eaea6c1
Ryo Narisawa <ororoauto@gmail.com>
2021-07-31 15:33:08 +0900

    ランキングの王冠を差し替え

--- lib/ui/assets/color/app_colors.dart
+++ lib/ui/assets/color/app_colors.dart
@@ -5,9 +5,6 @@ class AppColors {
   static const Color primaryLight = Color(0xffffd04c);
   static const Color primaryDark = Color(0xffc57000);
   static const Color onPrimary = Colors.white;
-  static const Color gold = Color(0xffAF9500);
-  static const Color silver = Color(0xffB4B4B4);
-  static const Color bronze = Color(0xffAD8A56);
   static const Color error = Color(0xff9b0000);
   static const Color note = Color(0xffFFE9BF);
 
