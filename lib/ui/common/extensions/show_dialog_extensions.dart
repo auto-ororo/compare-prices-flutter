5dd6a91ee710a39b46862d7fdca4d526eed31848
Ryo Narisawa <ororoauto@gmail.com>
2021-08-08 12:36:03 +0900

    showConfirmDialog 不要引数を削除

--- lib/ui/common/extensions/show_dialog_extensions.dart
+++ lib/ui/common/extensions/show_dialog_extensions.dart
@@ -4,24 +4,19 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 extension ShowDialogExtension on Widget {
   Future<T?> showConfirmDialog<T>(
       {required BuildContext context,
-      String? title,
       required String message,
-      required Function() onOk,
-      Function()? onCancel}) {
+      required Function() onOk}) {
     return showDialog<T>(
         context: context,
         builder: (_) {
           return AlertDialog(
-            title: Text(title ?? AppLocalizations.of(context)!.commonConfirm),
+            title: Text(AppLocalizations.of(context)!.commonConfirm),
             content: Text(message),
             actions: [
               // ボタン領域
               TextButton(
                   child: Text(AppLocalizations.of(context)!.commonCancel),
                   onPressed: () async {
-                    if (onCancel != null) {
-                      await onCancel();
-                    }
                     Navigator.pop(context);
                   }),
               TextButton(
